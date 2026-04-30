a930f1d8c902c68387d4f471ec62e0fee9628d7d
AryanTiwari44 <aryan44x@gmail.com>
2026-04-30 19:18:24 +0000

    converaion of dataypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strick" // treat all JS code as a new version
+// alert(3+3) // we are using nodejs, not brower
+//alert(3+3)
+console.log("aryan") // code readability should be high
+let name = "aryan"
+let age = 19
+let isLoggedin = false
+console.log(age)
+// number 
+//bigoint
+//string=>""
+//boolean=>true/falsew
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object=>
---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = '875'//created variable
+console.log(typeof score)//check the type
+let valueInNumber = Number(score)//convert into number
+console.log(typeof valueInNumber)//check the tyoe and also investigate 
+//'34'=>string
+//true=>1;false=>0
+//'123abc'=>NaN
+let someNumber = 33
+let valueInString = String(someNumber)
+console.log(valueInString)
+console.log(typeof valueInString)
+
+

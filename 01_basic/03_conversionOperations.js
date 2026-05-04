68820b110a00ee8be91d78085b975f58ba838a8d
AryanTiwari44 <aryan44x@gmail.com>
2026-05-04 18:07:48 +0000

    conersion

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,13 +1,46 @@
 let score = '875'//created variable
 console.log(typeof score)//check the type
 let valueInNumber = Number(score)//convert into number
-console.log(typeof valueInNumber)//check the tyoe and also investigate 
+console.log(typeof valueInNumber)//check the type and also investigate 
 //'34'=>string
 //true=>1;false=>0
 //'123abc'=>NaN
 let someNumber = 33
-let valueInString = String(someNumber)
+let valueInString = String(someNumber) //converted into string
 console.log(valueInString)
 console.log(typeof valueInString)
+//Operations
+let value = 5
+let negValue = -value
+console.log(negValue) //for negative value conversion
+// Arithmetic Operations
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+//addition
+let str1 = "Aryan"
+let str2 = " Tiwari"
+
+let str3 = str1+str2
+console.log(str3)
+
+
+//complex situations
+console.log(1 + "2")
+console.log('1' + 2)
+console.log('1' + 2 + 3)
+console.log(1 + 2 + '2')
+//prefix and postfix 
+ let gamecounter = 100
+ gamecounter++
+ console.log(gamecounter);
+ //if not understood this topic kindly visit mdn to study 
+ 
+
+
+
 
 

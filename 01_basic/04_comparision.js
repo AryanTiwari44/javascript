83c67b69fd1e5d5cc6da75c97410b96b6fa8ed5f
AryanTiwari44 <aryan44x@gmail.com>
2026-05-06 17:40:12 +0000

    comparision of datatypes

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,25 @@
+// basic of operators
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+
+//complex comparision
+console.log("2" > 3);
+console.log("92" > 4);
+//equality operator
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //here the equality operator works differently to other operators
+//same with undefined
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+// === stickly check
+console.log("2" === 6); //its check the both the datatypes 
+
+
+
+
+
+

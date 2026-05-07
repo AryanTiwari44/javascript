21301043576191e963bb142db1edbe25ae161544
AryanTiwari44 <aryan44x@gmail.com>
2026-05-07 17:40:45 +0000

    String

---
+++ 01_basic/06_string.js
@@ -0,0 +1,15 @@
+const name = "aryan"
+const valueCount = 76
+
+//console.log(name + valueCount + "value") // this type is very outdated avoid using this 
+
+//instead of you can you stirng interpolation using (``)this and ${}
+
+console.log(`my name is ${name} and my weight is ${valueCount}`)
+const gameName = new String("aryan") //another way to declare the string by new method and String name
+console.log(gameName[0])
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('y'));
+
+

24d13c0d927d329de8c30fef41fb72688cf101c7
AryanTiwari44 <aryan44x@gmail.com>
2026-05-07 18:28:55 +0000

    updated string file

--- 01_basic/06_string.js
+++ 01_basic/06_string.js
@@ -7,9 +7,16 @@ const valueCount = 76
 
 console.log(`my name is ${name} and my weight is ${valueCount}`)
 const gameName = new String("aryan") //another way to declare the string by new method and String name
+//methods of strings
 console.log(gameName[0])
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('y'));
-
+const newString = gameName.substring(0, 4) 
+console.log(newString)
+const anotherString = gameName.slice(-2, 4)
+console.log(anotherString);
+const newStringOne = "    aryan.       "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // important method to use in your website
 

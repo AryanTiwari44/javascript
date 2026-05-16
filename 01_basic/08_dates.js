57286addf00f6d6ebd3d3f197f8102721a6a15e6
AryanTiwari44 <aryan44x@gmail.com>
2026-05-16 15:20:27 +0000

    08_dates.js

---
+++ 01_basic/08_dates.js
@@ -0,0 +1,41 @@
+let myDate = new Date();
+console.log(myDate.toString()); //method toString()
+console.log(myDate.toJSON());
+let myCreatedDate = new Date(2006, 6, 17)
+console.log(myCreatedDate.toLocaleTimeString());
+    //mmddyy
+    let myCreatedDate2 = new Date("07-17-2006")
+console.log(myCreatedDate2.toString());
+let myTimestamp = Date.now();
+console.log(myTimestamp);
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
